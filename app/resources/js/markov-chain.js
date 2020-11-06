dd849ee52cd9890b3c7f1520aa9165639deab222
DimovDimo <d.dimov1996@gmail.com>
2020-11-06 10:52:40 +0200

    added functions getInput & splitInput

--- app/resources/js/markov-chain.js
+++ app/resources/js/markov-chain.js
@@ -1,5 +1,17 @@
 document.getElementById("generator").addEventListener("click", markov);
 
 function markov() {
-    
+    let chain = {};
+    let input = getInput();
+	let items = splitInput(input);
+}
+
+function getInput() {
+	return document.getElementById("input").value;
+}
+
+
+function splitInput(input) {
+	let splitParameter = document.getElementById("split").value;
+	return input.split(splitParameter);
 }
