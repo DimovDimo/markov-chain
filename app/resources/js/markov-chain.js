46f56d3de71fc325d70561e250ed538c7bae8feb
DimovDimo <d.dimov1996@gmail.com>
2020-11-09 20:27:10 +0200

    added functions getChain, newItem & relatedItem

--- app/resources/js/markov-chain.js
+++ app/resources/js/markov-chain.js
@@ -1,9 +1,32 @@
 document.getElementById("generator").addEventListener("click", markov);
 
 function markov() {
-    let chain = {};
-    let input = getInput();
+    let input = getInput().toLowerCase();
 	let items = getItems(input);
+	let chain = getChain(items);
+}
+
+function getChain(items) {
+	let chain = {};
+	for (let i = 0; i < items.length; i++) {
+		let item = items[i];
+		newItem(chain, item);
+		relatedItem(items, i, chain, item);
+	}
+
+	return chain;
+}
+
+function relatedItem(items, i, chain, item) {
+	if (items[i + 1]) {
+		chain[item].push(items[i + 1]);
+	}
+}
+
+function newItem(chain, item) {
+	if (!chain[item]) {
+		chain[item] = [];
+	}
 }
 
 function getInput() {
